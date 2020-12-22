5ccde27f9e8e3417cbabdd917c6facb4415b8650
Roberta <robertasciortino.ag@gmail.com>
2020-12-22 15:31:40 +0100

    partial header

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <title>@yield('title', 'Laravel Molisana')</title>
+  </head>
+  <body>
+    @include('partials.header')
+  </body>
+</html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,22 @@
+<header>
+  <div class="logo">
+    <img src="{{asset('images/logo-la-molisana.png')}}" alt="Logo la molisana">
+  </div>
+  <ul>
+    <li>
+      <a href="#">
+        Home
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        Prodotti
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        News
+      </a>
+    </li>
+  </ul>
+</header>
