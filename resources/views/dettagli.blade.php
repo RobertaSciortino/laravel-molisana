dd417100730635b4dd8d69a5f03bfa1c49c0ff74
Roberta <robertasciortino.ag@gmail.com>
2020-12-27 12:25:30 +0100

    pagine dettaglio prodotto completata

--- resources/views/dettagli.blade.php
+++ resources/views/dettagli.blade.php
@@ -4,10 +4,27 @@
 
 @section('content')
   <div class="product-details">
-    <h1> Prodotto con parametro = {{$shape['titolo']}} </h1>
+    <h1> {{$shape['titolo']}} </h1>
     <div class="product-image">
       <img src="{{$shape['src-h']}}" alt="{{$shape['titolo']}}">
       <img src="{{$shape['src-p']}}" alt="{{$shape['titolo']}}">
+    </div>
+    <div class="caption">
+      <div class="container">
+        <p>
+          {!!$shape['descrizione']!!}
+        </p>
+        <div class="shape-info">
+          <p>
+            <i class="far fa-clock"></i>
+            Tempi di cottura: {{$shape['cottura']}}
+          </p>
+          <p>
+            <i class="fas fa-balance-scale"></i>
+            Peso confezione: {{$shape['peso']}}
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,7 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
